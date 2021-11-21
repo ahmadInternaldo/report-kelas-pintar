eddb1773d3ea5d0b3d0cf2b87b406d010c3a9884
ahmadInternaldo <aldo@gigaming.io>
2021-11-22 02:14:51 +0700

    second initial

--- config/config.js
+++ config/config.js
@@ -10,7 +10,7 @@ module.exports = {
     port: 5432
   },
   "production": {
-    "use_env_variable":"DATABASE_URL",
+    "use_env_variable":process.env.DATABASE_URL,
     "ssl": true,
     "dialect": "postgres",
     "protocol": "postgres",
